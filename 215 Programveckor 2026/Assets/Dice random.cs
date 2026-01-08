77ac2b3b664544cc3ec399c2d604cf5366ee9450
Rex-drive <rex.mansson@elev.ga.lbs.se>
2026-01-08 14:10:38 +0100

    tärning och skit till tmp

---
+++ 215 Programveckor 2026/Assets/Dice random.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class Dicerandom : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))  // om det är din tur
+        {
+            rolldice();
+        }
+
+        void rolldice() 
+        {
+            int RandomDices = Random.Range(1, 7);
+            Console.WriteLine(RandomDices);
+        }
+    }
+}
