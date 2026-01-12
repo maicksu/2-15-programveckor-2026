c79a2b719f9008a97d62b8b966dbf74b66caa807
maicksu <maickel.sulaiman@elev.ga.lbs.se>
2026-01-12 13:49:08 +0100

    Lobby knapp script

---
+++ 215 Programveckor 2026/Assets/scripts/StartButton.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneToGame : MonoBehaviour
+{
+    public void GoTochoosecaracter()
+    {
+        SceneManager.LoadScene("choose caracter");
+
+    }
+
+}
