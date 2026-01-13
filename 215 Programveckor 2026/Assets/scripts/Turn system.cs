5a0bc3ee3a04e0197d93c98516bab39400d6b21d
Rex-drive <rex.mansson@elev.ga.lbs.se>
2026-01-13 14:17:29 +0100

    turn

--- 215 Programveckor 2026/Assets/scripts/Turn system.cs
+++ 215 Programveckor 2026/Assets/scripts/Turn system.cs
@@ -12,7 +12,7 @@ public class Turnsystem : MonoBehaviour
 
     Vector2Int playerposition = new Vector2Int(0, 0);
 
-    public List<Vector2Int> playerpositions = new List<Vector2Int>();
+    public List<Vector2Int> TilePosition = new List<Vector2Int>();
 
 
 
@@ -31,71 +31,68 @@ public class Turnsystem : MonoBehaviour
         playerturns.Add(player3);
         playerturns.Add(player4);
 
-        playerpositions.Add(new Vector2Int(0, 0)); //[0] också vidare
-        playerpositions.Add(new Vector2Int(1, 0)); //[1]
-        playerpositions.Add(new Vector2Int(2, 0));
-        playerpositions.Add(new Vector2Int(3, 0));
-        playerpositions.Add(new Vector2Int(4, 0));
-        playerpositions.Add(new Vector2Int(5, 0));
-        playerpositions.Add(new Vector2Int(6, 0));
-        playerpositions.Add(new Vector2Int(7, 0));
-        playerpositions.Add(new Vector2Int(8, 0));
-        playerpositions.Add(new Vector2Int(9, 0));
-        playerpositions.Add(new Vector2Int(10, 0));
-        playerpositions.Add(new Vector2Int(11, 0));
-        playerpositions.Add(new Vector2Int(12, 0));
-        playerpositions.Add(new Vector2Int(13, 0));
-        playerpositions.Add(new Vector2Int(14, 0)); //[14]
+        TilePosition.Add(new Vector2Int(0, 0)); //[0] också vidare
+        TilePosition.Add(new Vector2Int(1, 0)); //[1]
+        TilePosition.Add(new Vector2Int(2, 0));
+        TilePosition.Add(new Vector2Int(3, 0));
+        TilePosition.Add(new Vector2Int(4, 0));
+        TilePosition.Add(new Vector2Int(5, 0));
+        TilePosition.Add(new Vector2Int(6, 0));
+        TilePosition.Add(new Vector2Int(7, 0));
+        TilePosition.Add(new Vector2Int(8, 0));
+        TilePosition.Add(new Vector2Int(9, 0));
+        TilePosition.Add(new Vector2Int(10, 0));
+        TilePosition.Add(new Vector2Int(11, 0));
+        TilePosition.Add(new Vector2Int(12, 0));
+        TilePosition.Add(new Vector2Int(13, 0));
+        TilePosition.Add(new Vector2Int(14, 0)); //[14]
 
-        playerpositions.Add(new Vector2Int(14, -1));
-        playerpositions.Add(new Vector2Int(14, -2));
-        playerpositions.Add(new Vector2Int(14, -3));
-        playerpositions.Add(new Vector2Int(14, -4));
-        playerpositions.Add(new Vector2Int(14, -5));
-        playerpositions.Add(new Vector2Int(14, -6));
-        playerpositions.Add(new Vector2Int(14, -7));
-        playerpositions.Add(new Vector2Int(14, -8));
-        playerpositions.Add(new Vector2Int(14, -9));
-        playerpositions.Add(new Vector2Int(14, -10));
-        playerpositions.Add(new Vector2Int(14, -11));
-        playerpositions.Add(new Vector2Int(14, -12));
-        playerpositions.Add(new Vector2Int(14, -13));
-        playerpositions.Add(new Vector2Int(14, -14)); //[28]
+        TilePosition.Add(new Vector2Int(14, -1));
+        TilePosition.Add(new Vector2Int(14, -2));
+        TilePosition.Add(new Vector2Int(14, -3));
+        TilePosition.Add(new Vector2Int(14, -4));
+        TilePosition.Add(new Vector2Int(14, -5));
+        TilePosition.Add(new Vector2Int(14, -6));
+        TilePosition.Add(new Vector2Int(14, -7));
+        TilePosition.Add(new Vector2Int(14, -8));
+        TilePosition.Add(new Vector2Int(14, -9));
+        TilePosition.Add(new Vector2Int(14, -10));
+        TilePosition.Add(new Vector2Int(14, -11));
+        TilePosition.Add(new Vector2Int(14, -12));
+        TilePosition.Add(new Vector2Int(14, -13));
+        TilePosition.Add(new Vector2Int(14, -14)); //[28]
 
-        playerpositions.Add(new Vector2Int(13, -14));
-        playerpositions.Add(new Vector2Int(12, -14));
-        playerpositions.Add(new Vector2Int(11, -14));
-        playerpositions.Add(new Vector2Int(10, -14));
-        playerpositions.Add(new Vector2Int(9, -14));
-        playerpositions.Add(new Vector2Int(8, -14));
-        playerpositions.Add(new Vector2Int(7, -14));
-        playerpositions.Add(new Vector2Int(6, -14));
-        playerpositions.Add(new Vector2Int(5, -14));
-        playerpositions.Add(new Vector2Int(4, -14));
-        playerpositions.Add(new Vector2Int(3, -14));
-        playerpositions.Add(new Vector2Int(2, -14));
-        playerpositions.Add(new Vector2Int(1, -14));
-        playerpositions.Add(new Vector2Int(0, -14)); //[42]
+        TilePosition.Add(new Vector2Int(13, -14));
+        TilePosition.Add(new Vector2Int(12, -14));
+        TilePosition.Add(new Vector2Int(11, -14));
+        TilePosition.Add(new Vector2Int(10, -14));
+        TilePosition.Add(new Vector2Int(9, -14));
+        TilePosition.Add(new Vector2Int(8, -14));
+        TilePosition.Add(new Vector2Int(7, -14));
+        TilePosition.Add(new Vector2Int(6, -14));
+        TilePosition.Add(new Vector2Int(5, -14));
+        TilePosition.Add(new Vector2Int(4, -14));
+        TilePosition.Add(new Vector2Int(3, -14));
+        TilePosition.Add(new Vector2Int(2, -14));
+        TilePosition.Add(new Vector2Int(1, -14));
+        TilePosition.Add(new Vector2Int(0, -14)); //[42]
 
-        playerpositions.Add(new Vector2Int(0, -13));
-        playerpositions.Add(new Vector2Int(0, -12));
-        playerpositions.Add(new Vector2Int(0, -11));
-        playerpositions.Add(new Vector2Int(0, -10));
-        playerpositions.Add(new Vector2Int(0, -9));
-        playerpositions.Add(new Vector2Int(0, -8));
-        playerpositions.Add(new Vector2Int(0, -7));
-        playerpositions.Add(new Vector2Int(0, -6));
-        playerpositions.Add(new Vector2Int(0, -5));
-        playerpositions.Add(new Vector2Int(0, -4));
-        playerpositions.Add(new Vector2Int(0, -3));
-        playerpositions.Add(new Vector2Int(0, -2));
-        playerpositions.Add(new Vector2Int(0, -1)); //[55] inte hörn btw det är [0] som är det
+        TilePosition.Add(new Vector2Int(0, -13));
+        TilePosition.Add(new Vector2Int(0, -12));
+        TilePosition.Add(new Vector2Int(0, -11));
+        TilePosition.Add(new Vector2Int(0, -10));
+        TilePosition.Add(new Vector2Int(0, -9));
+        TilePosition.Add(new Vector2Int(0, -8));
+        TilePosition.Add(new Vector2Int(0, -7));
+        TilePosition.Add(new Vector2Int(0, -6));
+        TilePosition.Add(new Vector2Int(0, -5));
+        TilePosition.Add(new Vector2Int(0, -4));
+        TilePosition.Add(new Vector2Int(0, -3));
+        TilePosition.Add(new Vector2Int(0, -2));
+        TilePosition.Add(new Vector2Int(0, -1)); //[55] inte hörn btw det är [0] som är det
 
     }
-    public void PositionsOnPlayers()
-    {
-        
-    }
+
     public void Turn()
     {
         if (Input.GetKeyUp(KeyCode.Space))
@@ -103,18 +100,66 @@ public class Turnsystem : MonoBehaviour
 
             int movement = dicerandom.rolldice();
 
-            Vector2Int CornerNW = playerpositions[0];
-            Vector2Int CornerNE = playerpositions[14];
-            Vector2Int CornerSE = playerpositions[28];
-            Vector2Int CornerSW = playerpositions[42];
+            Vector2Int CornerNW = TilePosition[0];
+            Vector2Int CornerNE = TilePosition[14];
+            Vector2Int CornerSE = TilePosition[28];
+            Vector2Int CornerSW = TilePosition[42];
 
-            Vector2Int NorthEdge = playerpositions[1-13];
-            Vector2Int EastEdge = playerpositions[14-27];
-            Vector2Int SouthEdge = playerpositions[29-41];
-            Vector2Int WestEdge = playerpositions[43-55];
+            Vector2Int NorthEdge = TilePosition[1-13];
+            Vector2Int EastEdge = TilePosition[14-27];
+            Vector2Int SouthEdge = TilePosition[29-41];
+            Vector2Int WestEdge = TilePosition[43-55];
 
+            if(CornerNW == playerposition)
+            {
+                if (Input.GetKey(KeyCode.D)) playerposition.x += movement;
+                if (Input.GetKey(KeyCode.S)) playerposition.y -= movement;
 
-            
+            }
+            if (CornerNE == playerposition)
+            {
+                if (Input.GetKey(KeyCode.A)) playerposition.x -= movement;
+                if (Input.GetKey(KeyCode.S)) playerposition.y -= movement;
+
+            }
+            if (CornerSE == playerposition)
+            {
+                if (Input.GetKey(KeyCode.A)) playerposition.x -= movement;
+                if (Input.GetKey(KeyCode.W)) playerposition.y += movement;
+
+            }
+            if (CornerSW == playerposition)
+            {
+                if (Input.GetKey(KeyCode.D)) playerposition.x += movement;
+                if (Input.GetKey(KeyCode.W)) playerposition.y += movement;
+
+            }
+
+            if(NorthEdge == playerposition)
+            {
+                if (Input.GetKey(KeyCode.D)) playerposition.x += movement;
+                if (Input.GetKey(KeyCode.A)) playerposition.x -= movement;
+
+            }
+            if (EastEdge == playerposition)
+            {
+                if (Input.GetKey(KeyCode.S)) playerposition.y -= movement;
+                if (Input.GetKey(KeyCode.W)) playerposition.y += movement;
+
+            }
+            if (SouthEdge == playerposition)
+            {
+                if (Input.GetKey(KeyCode.D)) playerposition.x += movement;
+                if (Input.GetKey(KeyCode.A)) playerposition.x -= movement;
+
+            }
+            if (WestEdge == playerposition)
+            {
+                if (Input.GetKey(KeyCode.S)) playerposition.y -= movement;
+                if (Input.GetKey(KeyCode.W)) playerposition.y += movement;
+
+            }
+
 
             boardSpace.OnLand();
             
